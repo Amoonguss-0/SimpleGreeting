bd7614c2b04bd53af6fb4a9eeeec4857040c3120
Justin-F5972 <justinfernandez27@outlook.com>
2022-01-05 14:07:42 -0800

    Added a third question and fixed the second questions loop

--- SimpleGreeting/Program.cs
+++ SimpleGreeting/Program.cs
@@ -4,11 +4,20 @@ string input1 = Console.ReadLine();
 Console.WriteLine(input1 + " what is your favorite color");
 string input2 = Console.ReadLine();
 
-if(input2 == "purple" && input2 == "Purple")
+if(input2 == "purple" || input2 == "Purple")
 {
     Console.WriteLine(input2 + " huh you have good tastes");
 }
 else
 {
-    Console.WriteLine(input2 + "huh Purple is a better color");
+    Console.WriteLine(input2 + " huh Purple is a better color");
 }
+
+Console.WriteLine(input1 + " do you scalp computer parts");
+string input3 = Console.ReadLine();
+
+if(input3 == "yes" || input3 == "Yes" || input3 == "y")
+{
+    Console.WriteLine("Thats not very cool of you " + input1 + " you should sell me a RTX 3080 at MSRP though. Ill pay for taxes as well.");
+}
+Console.ReadKey();
