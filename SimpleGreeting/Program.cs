17af11c80b48a6c2761f944f6a32564b82b5b43a
Justin-F5972 <justinfernandez27@outlook.com>
2022-01-05 13:58:32 -0800

    Added a message and new question

--- SimpleGreeting/Program.cs
+++ SimpleGreeting/Program.cs
@@ -1,2 +1,5 @@
 ﻿Console.WriteLine("What is your name");
-string input = Console.ReadLine();
+string input1 = Console.ReadLine();
+
+Console.WriteLine(input1 + " what is your favorite color");
+string input2 = Console.ReadLine();
