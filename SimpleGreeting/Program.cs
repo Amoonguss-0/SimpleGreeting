5b3d6e5b2684887c4fcfc5f729746524fc8211a4
Justin-F5972 <justinfernandez27@outlook.com>
2022-01-05 13:59:48 -0800

    Added the first loop that will reply to your second choice

--- SimpleGreeting/Program.cs
+++ SimpleGreeting/Program.cs
@@ -3,3 +3,8 @@ string input1 = Console.ReadLine();
 
 Console.WriteLine(input1 + " what is your favorite color");
 string input2 = Console.ReadLine();
+
+if(input2 == "purple" && input2 == "Purple")
+{
+    Console.WriteLine(input2 + " huh you have good tastes");
+}
